a0e1bdb2095eee5b61ff49cd1f5ceced00f0fa50
Alejandro Aldea <alejandro.aldea@usach.cl>
2023-10-05 21:18:19 -0300

    [Alejandro Aldea] - utilización del CRUD usuario

--- src/main/java/com/Grupo6/Control2/repositories/UsuarioRepository.java
+++ src/main/java/com/Grupo6/Control2/repositories/UsuarioRepository.java
@@ -2,7 +2,12 @@ package com.Grupo6.Control2.repositories;
 
 import com.Grupo6.Control2.models.Usuario;
 
+import java.util.List;
+
 public interface UsuarioRepository {
     public Usuario crearUsuario(Usuario usuario);
+    public boolean borrarUsuario(Long id_usuario);
+    public boolean actualizarUsuario(Usuario usuario);
+    public List<Usuario> getUsuarios();
 
 }
